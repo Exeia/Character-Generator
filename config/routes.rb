7cec2d86c96c54a6b91804b62d009beb42504c87
peter <pkeoman17@gmail.com>
2014-11-30 20:35:30 -0800

    Worked on view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'	
+  root 'static_pages#thome'	
   get 'static_pages/about'
   
   # The priority is based upon order of creation: first created -> highest priority.
