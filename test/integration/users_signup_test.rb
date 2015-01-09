70868a95b0dbacfe30fb6ce20c497c93e1ac99aa
peter <pkeoman17@gmail.com>
2015-01-08 22:15:36 -0800

    login and out implement

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,13 +3,14 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
     test "invalid signup information" do 
         get signup_path
-        assert_no_difference 'User.count' do 
+        assert_no_difference 'User.count', 1 do 
             post users_path, user: { name: "",
                                      email: "user@invalid",
-                                     handle: "",
+                                     handle: "Hello",
                                      password:	"foo",
                                      password_confirmation: "bar"}
         end
         assert_template 'user/new'
+        assert is_logged_in?
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,29 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  def setup
+      @user = users(:michael)
+	  remember(@user)
+    
+  end
   # Add more helper methods to be used by all tests here...
+  def log_in_as(user, options = {})
+      password = options[:password] || 'password'
+      remember_me = options[:remember_me] || '1'
+      if integration_test?
+          post login_path, session: { handle: user.handle,
+                                      password:  password,
+                                      remember_me: remember_me }
+      else
+          session[:used_id] = user.id 
+      end
+  end
+
+ private
+
+ def integration_test?
+     defined?(post_via_redirect)
+     
+ end
+
 end
