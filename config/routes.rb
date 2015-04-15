4aaabe4b41d783fec873df1ec33848faa838cd12
peter <pkeoman17@gmail.com>
2015-04-14 23:16:13 -0700

    Characer generator init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'characters/new'
+
+  get 'characters/show'
+
+  get 'new/show'
+
   get 'users/new'
 
   root 'static_pages#home'	
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105032713) do
+ActiveRecord::Schema.define(version: 20150414050901) do
+
+  create_table "characters", force: true do |t|
+    t.string   "name"
+    t.string   "job"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
